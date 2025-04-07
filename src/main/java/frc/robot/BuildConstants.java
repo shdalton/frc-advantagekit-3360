b42f18fc0fb987ee431911b5ccd8c14455392d72
Stephane Dalton <stephane.h.dalton@gmail.com>
2025-04-07 19:13:24 -0400

    initial import

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_TalonSwerveTemplate_EnhancedPhoenixSimulation-Imported";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 482;
+  public static final String GIT_SHA = "68d1f6246fe72a0c90b7a8fe2bd7f96344b7316e";
+  public static final String GIT_DATE = "2025-03-28 03:18:34 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-04-07 19:01:54 EDT";
+  public static final long BUILD_UNIX_TIME = 1744066914513L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
